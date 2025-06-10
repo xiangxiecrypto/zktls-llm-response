0071301bdf659af074d7a2c2975f4033709e1a34
Xiang Xie <xiexiangiscas@gmail.com>
2025-06-10 17:37:44 +0800

    refine

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 async function primusProofTest() {
     // Initialize parameters, the init function is recommended to be called when the program is initialized.
     const appId = "0x21c44c75014fc8a104ff139f24f60c59c3c3256c";
-    const appSecret = "0x49b0e4bfe2b9566830f435267b921b45280cf512bead503fd061f9fcb52b117e"; // Just for testing, appSecret cannot be written in the front-end code
+    const appSecret = "0x49b0e4bfe2b9566830f435267b921b45280cf512bead503fd061f9fcb52b117e";
     const zkTLS = new PrimusCoreTLS();
     const initResult = await zkTLS.init(appId, appSecret);
     console.log("primusProof initResult=", initResult);
@@ -29,8 +29,8 @@ async function primusProofTest() {
 
     const responseResolves = [
         {
-            keyName: 'response', // According to the response keyname, such as: instType.
-            parsePath: '$.choices[0].message.content', // According to the response parsePath, such as: $.data[0].instType.
+            keyName: 'response', // Set a name according to the response keyname.
+            parsePath: '$.choices[0].message.content', // Set the value you want to prove according to the response parsePath.
         }
     ];
     // Generate attestation request.
@@ -47,14 +47,6 @@ async function primusProofTest() {
 
     const verifyResult = zkTLS.verifyAttestation(attestation);
     console.log("verifyResult=", verifyResult);
-    if (verifyResult === true) {
-        console.log("Attestation verification succeeded.");
-        // Business logic checks, such as attestation content and timestamp checks
-        // do your own business logic.
-    } else {
-        console.log("Attestation verification failed.");
-        // If failed, define your own logic.
-    }
 
     process.exit(0);
 }
